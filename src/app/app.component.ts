a26f2911f8872eb7d4eed1d44f6474dd6d72d721
Suresh Nagar <nagars@vmware.com>
2021-08-01 15:24:16 -0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { arc } from 'd3-shape';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,41 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'standup-wheel';
+
+  arcPath: string | null = '';
+
+  NUM_PERSONS = 10;
+
+  persons: { name: string, startAngle: number, endAngle: number, color: string, path?: string | null }[] = [];
+
+  constructor() {
+    this.fillPersons();
+    this.generatePath();
+  }
+
+  fillPersons(): void {
+    const increment = (Math.PI * 2) / this.NUM_PERSONS;
+    let startAngle = 0;
+    for (let i = 0; i < this.NUM_PERSONS; i++) {
+      this.persons.push({
+        name: `Person ${i}`,
+        startAngle: startAngle,
+        endAngle: (startAngle + increment),
+        color: `#${Math.floor(Math.random() * 16777215).toString(16)}`
+      });
+      startAngle += increment;
+    }
+  }
+  generatePath(): void {
+    const arcFn = arc();
+    this.persons.forEach(person => {
+      person.path = arcFn({
+        innerRadius: 10,
+        outerRadius: 100,
+        startAngle: person.startAngle,
+        endAngle: person.endAngle,
+      });
+    });
+  }
+
 }
